38c443b033a11dc876dd3b57b933b9ba61a3ea92
riyam2309 <141444622+riyam2309@users.noreply.github.com>
2023-10-04 19:39:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+const nav=document.getElementById('navbar');
 
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+  payment.style.display = "none";
+});
